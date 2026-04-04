c827c18b15f5ac96d2ae43e7898a11618fb76e4c
charles <perignessanga3@awdpay.com>
2026-04-04 05:40:53 +0100

    ajout lien vercel dans setting.py projet pet a 94% du cahier de charges

--- backend/evisa_backend/settings.py
+++ backend/evisa_backend/settings.py
@@ -198,7 +198,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "https://charles237.pythonanywhere.com",
-    # "https://your-vercel-app.vercel.app",  # TODO: Ajouter l'URL Vercel ici
+    "https://evisa-cameroun.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
