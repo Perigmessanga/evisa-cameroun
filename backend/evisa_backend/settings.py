2f35e203b67ff51c82336410ca95ae87861aea15
perigmessanga <messangaperig3@gmail.com>
2026-05-12 10:41:23 +0100

    remove sslserver dans event_backend/settings.py

--- backend/evisa_backend/settings.py
+++ backend/evisa_backend/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'django_filters',
-    'sslserver',
+    # 'sslserver',
 
     # Applications locales
     'apps.users',
